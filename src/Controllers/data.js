abc21f0a6578dfa39715d17c65518d188b10e35a
Sayan625 <sayanguha625@gmail.com>
2023-11-01 23:42:45 +0530

    changed api base

--- src/Controllers/data.js
+++ src/Controllers/data.js
@@ -1,2 +1,3 @@
 // global variable for the entire app
-export const apiBase='http://localhost:8000/api'
+// export const apiBase='http://localhost:8000/api'
+export const apiBase='https://nure-backend.onrender.com/api'
